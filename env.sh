c1fcadd2e5e4010ed5609bd4d09bf20d86685b57
farfromrefuge <dev@akylas.fr>
2022-11-26 14:23:34 +0100

    chore: fixes

--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export PATH=$SCRIPT_DIR/venv/bin:$SCRIPT_DIR/tippecanoe:$SCRIPT_DIR/valhalla/build:$SCRIPT_DIR/prime_server:$PATH
 export OUTPUT_DIR=alpimaps_mbtiles
-export PLANETILER_JAR=$SCRIPT_DIR/planetiler-dist/target/planetiler-dist-0.5-SNAPSHOT-with-deps.jar
+export PLANETILER_JAR=$SCRIPT_DIR/planetiler/planetiler-dist/target/planetiler-dist-0.5-SNAPSHOT-with-deps.jar
